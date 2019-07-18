1be8a0d0e84579afe439ac079a4910dc9052622f
Dileep Dayanand Pai <dilludp123@gmail.com>
2019-07-18 14:32:11 +0530

    removed data from appconfig

--- sample/Kmd.Logic.Sms.Client.Sample/AppConfigurations.cs
+++ sample/Kmd.Logic.Sms.Client.Sample/AppConfigurations.cs
@@ -6,14 +6,14 @@ namespace Kmd.Logic.Sms.Client.Sample
 {
     public static class AppConfigurations
     {
-        public const string Token = @"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6InpGTDB5ZGF2NDgxSHJDT2M0bk9MdTR2cDB2SDZxeTBvUS1SdlBzQW1vNlkifQ.eyJpc3MiOiJodHRwczovL2xvZ2ljaWRlbnRpdHlwcm9kLmIyY2xvZ2luLmNvbS9hYTBkNmYzYi04ZmIyLTQ3N2QtYWI4OC1iYjJlNjY0NjIwZTgvdjIuMC8iLCJleHAiOjE1NjM0NDE1MDAsIm5iZiI6MTU2MzQzNzkwMCwiYXVkIjoiZDM0MmY1MDctOWE0Ny00YzZhLTg1ZDktYTkyNTNjMzllNjYyIiwiZ2l2ZW5fbmFtZSI6IkRpbGVlcCBEYXlhbmFuZCIsImZhbWlseV9uYW1lIjoiUGFpIiwiZW1haWwiOiJxdnhAa21kLmRrIiwibXVuaWNpcGFsaXR5IjoiODk5IiwiYXV0aGVudGljYXRpb25Tb3VyY2UiOiJLTUQiLCJuYW1lIjoiRGlsZWVwIERheWFuYW5kIFBhaSIsInN1YiI6IjhkZWFmYzgzLTBjODEtNDY1Mi05ZGFhLWVjMmNkYWZhMDJkMyIsInJvbGVzIjpbXSwicGVybWlzc2lvbnMiOltdLCJub25jZSI6ImRlZmF1bHROb25jZSIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsImF6cCI6ImYwMWE3MmQ3LWEyN2UtNGMyZi1hMDFmLWE4NDBkMTBjODRhNCIsInZlciI6IjEuMCIsImlhdCI6MTU2MzQzNzkwMH0.WDw8UXqNuVP6I9KrJ5g-twW5pjiO3O0VYnnVzvnxgSJrqcO1HhK5mheRsk7hNwvva31UyWC1Wa_CB12v182KRyJVlyPuIzNWJqTpvM65DtagWi8M9TEKzUVF2RHURAWjAFdTDOGGPHW2OM-lo4SqyZG-ugIJxIkH55v_cx24aYF5PoHD_JjUSOJZMsnMjOISJOis86j2ZMxgZ8nKb9WI303CPU6Uj5Fzvxnpbm1DrZMk7ekDhgO0ZQ75-_8ifIQ-b1xnRnCfYT90fy7H5Vh5orqrFL4awp232HDCy1W3eJRihbrpejg6mX2yKzBrF4NKjM76wP_U9uMz6FzLagddwA"; // bearer token
-        public static Guid SubscriptionId = Guid.Parse("b8ed0d06-b7f1-42c3-a811-057849c2d6e4"); // subscription ID
+        public const string Token = @""; // bearer token
+        public static Guid SubscriptionId = Guid.Parse(""); // subscription ID
         public const string ApiKey = "";
         public const string Sender = "";
-        public const string Username = "ACce6f3145aee249a84f3b2edb971cedc0";
-        public const string AccountSid = "ACce6f3145aee249a84f3b2edb971cedc0";
-        public const string FromProperty = "+13479466088";
-        public const string ToPhoneNumber = "+919742122499";
-        public const string Password = "b956223f604e2435b8d6ae23124f83f9";
+        public const string Username = "";
+        public const string AccountSid = "";
+        public const string FromProperty = "";
+        public const string ToPhoneNumber = "";
+        public const string Password = "";
     }
 }
