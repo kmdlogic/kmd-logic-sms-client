de43139d184d0f31adff60e2d085199fea3b5073
Adam Chester <adamchester@gmail.com>
2019-08-06 13:36:48 +1000

    Use command line options and console logger

---
+++ sample/Kmd.Logic.Sms.Client.Sample/CommandLineAction.cs
@@ -0,0 +1,10 @@
+namespace Kmd.Logic.Sms.Client.Sample
+{
+    public enum CommandLineAction
+    {
+        CreateTwilioConfig,
+        CreateLinkMobilityConfig,
+        CreateLogicConfig,
+        SendSms,
+    }
+}
