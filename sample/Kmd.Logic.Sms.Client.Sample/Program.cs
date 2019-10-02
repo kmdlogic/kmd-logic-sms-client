6df51fed438107b75302619fc9486d2c5a1e102a
Adam Chester <adamchester@gmail.com>
2019-10-02 11:23:56 +1000

    Improved sample

--- sample/Kmd.Logic.Sms.Client.Sample/Program.cs
+++ sample/Kmd.Logic.Sms.Client.Sample/Program.cs
@@ -82,23 +82,43 @@ namespace Kmd.Logic.Sms.Client.Sample
             var client = GetApi(config);
 
             var resultTwilioProvider = client.CreateTwilioProviderConfiguration(
-               subscriptionId: config.SubscriptionId,
-               request: new ProviderConfigurationRequestTwilioProviderConfig(
-                   displayName: "SmsClientSampleTwilio",
-                   new TwilioProviderConfig(
-                       username: config.TwilioUsername,
-                       password: config.TwilioPassword,
-                       accountSid: config.TwilioAccountSid,
-                       fromProperty: config.TwilioFromProperty,
-                       smsServiceWindow: null),
-                   new SendTestSmsRequest(
-                       toPhoneNumber: config.ToPhoneNumber,
-                       body: config.SmsBody)));
+                subscriptionId: config.SubscriptionId,
+                request: new ProviderConfigurationRequestTwilioProviderConfig(
+                    displayName: "SmsClientSampleTwilio",
+                    new TwilioProviderConfig(
+                        username: config.TwilioUsername,
+                        password: config.TwilioPassword,
+                        accountSid: config.TwilioAccountSid,
+                        fromProperty: config.TwilioFromProperty,
+                        smsServiceWindow: null),
+                    new SendTestSmsRequest(
+                        toPhoneNumber: config.ToPhoneNumber,
+                        body: config.SmsBody)));
 
             Log.Information("Created provider config {@ProviderConfig}", resultTwilioProvider);
-            Guid providerConfigurationId = (resultTwilioProvider as ProviderConfigurationResponseTwilioProviderConfig).ProviderConfigurationId;
+            return (resultTwilioProvider as ProviderConfigurationResponseTwilioProviderConfig)?.ProviderConfigurationId ?? Guid.Empty;
+        }
 
-            return providerConfigurationId;
+        private static Guid CreateFakeConfiguration(CommandLineConfig config)
+        {
+            var client = GetApi(config);
+
+            var resultTwilioProvider = client.CreateFa(
+                subscriptionId: config.SubscriptionId,
+                request: new ProviderConfigurationRequestTwilioProviderConfig(
+                    displayName: "SmsClientSampleTwilio",
+                    new TwilioProviderConfig(
+                        username: config.TwilioUsername,
+                        password: config.TwilioPassword,
+                        accountSid: config.TwilioAccountSid,
+                        fromProperty: config.TwilioFromProperty,
+                        smsServiceWindow: null),
+                    new SendTestSmsRequest(
+                        toPhoneNumber: config.ToPhoneNumber,
+                        body: config.SmsBody)));
+
+            Log.Information("Created provider config {@ProviderConfig}", resultTwilioProvider);
+            return (resultTwilioProvider as ProviderConfigurationResponseTwilioProviderConfig)?.ProviderConfigurationId ?? Guid.Empty;
         }
 
         private static Guid CreateLinkMobilityProviderConfiguration(CommandLineConfig config)
@@ -116,9 +136,7 @@ namespace Kmd.Logic.Sms.Client.Sample
                         body: config.SmsBody)));
 
             Log.Information("Created provider config {@ProviderConfig}", resultLinkMobilityProvider);
-            Guid providerConfigurationId = (resultLinkMobilityProvider as ProviderConfigurationResponseLinkMobilityProviderConfig).ProviderConfigurationId;
-
-            return providerConfigurationId;
+            return (resultLinkMobilityProvider as ProviderConfigurationResponseLinkMobilityProviderConfig)?.ProviderConfigurationId ?? Guid.Empty;
         }
 
         private static Guid CreateLogicConfiguration(CommandLineConfig config)
@@ -133,9 +151,7 @@ namespace Kmd.Logic.Sms.Client.Sample
                        smsServiceWindow: null)));
 
             Log.Information("Created provider config {@ProviderConfig}", resultLogicProvider);
-            Guid providerConfigurationId = (resultLogicProvider as ProviderConfigurationResponseLogicProviderConfig).ProviderConfigurationId;
-
-            return providerConfigurationId;
+            return (resultLogicProvider as ProviderConfigurationResponseLogicProviderConfig)?.ProviderConfigurationId ?? Guid.Empty;
         }
 
         private static void SendSmsBatch(CommandLineConfig config)
@@ -155,7 +171,8 @@ namespace Kmd.Logic.Sms.Client.Sample
                             toPhoneNumber: config.ToPhoneNumber,
                             body: config.SmsBody,
                             callbackUrl: $"{config.CallbackUri}",
-                            providerConfigurationId: config.ProviderConfigurationId)).GetAwaiter().GetResult())
+                            providerConfigurationId: config.ProviderConfigurationId)))
+                .Select(task => task.GetAwaiter().GetResult())
                 .ToArray();
 
             var summary = new
@@ -168,7 +185,7 @@ namespace Kmd.Logic.Sms.Client.Sample
                     {
                         HttpStatusCode = g.Key,
                         TotalCount = g.Count(),
-                        First3 = g.Take(3).Select(ResponseAsLogable).ToArray()
+                        First3 = g.Take(3).Select(ResponseAsLogable).ToArray(),
                     })
                     .ToArray(),
             };
@@ -176,7 +193,7 @@ namespace Kmd.Logic.Sms.Client.Sample
             Log.Information("Sent SMS with results {@Results}", summary);
         }
 
-        static object ResponseAsLogable(HttpOperationResponse<object> response)
+        private static object ResponseAsLogable(HttpOperationResponse<object> response)
         {
             return new
             {
@@ -198,7 +215,7 @@ namespace Kmd.Logic.Sms.Client.Sample
                     providerConfigurationId: config.ProviderConfigurationId));
 
             Log.Information("Sent SMS and got result {@Result}", sendSmsResult);
-            return (sendSmsResult as SendSmsResponse).SmsMessageId;
+            return (sendSmsResult as SendSmsResponse)?.SmsMessageId ?? Guid.Empty;
         }
     }
 }
